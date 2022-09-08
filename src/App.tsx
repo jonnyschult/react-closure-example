b1f2b49140dc9af03e8d15190461581d9c6dbbc6
jonnyschult <66886379+jonnyschult@users.noreply.github.com>
2022-09-08 10:32:31 -0500

    initial setup for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  let timer: NodeJS.Timer;
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      console.log(count);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []); // add count to dep array to fix wrong console log
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p style={{ marginBottom: 30 }}>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+ 1 to count</button>
     </div>
   );
 }
